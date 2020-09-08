c04bd2bb93572f87e3f0d5993439930434df01f7
OWamsley <47764303+OWamsley@users.noreply.github.com>
2020-09-08 13:24:19 -0600

    uploading static file
    need to learn how to access static files

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 # Template directory setting
 
 # TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
