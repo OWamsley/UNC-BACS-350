5a3f340eabf0cab53f4f6dfe5a24a336d4d9b3b6
OWamsley <47764303+OWamsley@users.noreply.github.com>
2020-09-10 10:02:39 -0600

    Added superheroes
    added hawkeye and rorschach pages +  static

--- app2/hero/urls.py
+++ app2/hero/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import AboutView, HomeView, ProfileView
+from .views import *
 
 urlpatterns = [
     path('about/', AboutView.as_view()),
     path('home', HomeView.as_view()),
     path('', HomeView.as_view()),
     path('profile/', ProfileView.as_view()),
+    path('hawkeye/', HawkeyeView.as_view()),
+    path('rorschach/', RorschachView.as_view()),
+
 ]
--- app2/hero/views.py
+++ app2/hero/views.py
@@ -3,24 +3,40 @@ from django.views.generic import TemplateView
 
 class AboutView(TemplateView):
     template_name="page.html"
-    def get_contect_data(self, **kwargs):
+    def get_context_data(self, **kwargs):
         return{
             'title': 'About Page',
             'body': 'about page',
         }
     
 class HomeView(TemplateView):
-    template_name="page.html"
-    def get_contect_data(self, **kwargs):
+    template_name="home.html"
+    def get_context_data(self, **kwargs):
         return{
-            'title': 'Home Page',
-            'body': 'home page',
+            'title': 'Home',
+            'info': 'Learn about superheroes!',
+            
         }
     
 class ProfileView(TemplateView):
     template_name="page.html"
-    def get_contect_data(self, **kwargs):
+    def get_context_data(self, **kwargs):
         return{
             'title': 'Title',
             'body': 'body',
+        }
+
+class HawkeyeView(TemplateView):
+    template_name="page.html"
+    def get_context_data(self, **kwargs):
+        return{
+            'title': 'Hawkeye',
+            'info': 'Hawkeye is Clint Barton, and he is a master marksman. He is cooler in the comics than he is in the movies.',
+        }
+class RorschachView(TemplateView):
+    template_name="page.html"
+    def get_context_data(self, **kwargs):
+        return{
+            'title': 'Rorschach',
+            'info': 'Rorschach is an anti-hero from Watchmen, a deconstruction of the idea of superheroes. Rorschach is a vigilante that believes in black and white good and evil.',
         }
