ba069dbbdc24cbf635fce0d71a38aefd058d659e
OWamsley <47764303+OWamsley@users.noreply.github.com>
2020-11-30 21:19:32 -0700

    Added User
    login, signup, logout

--- Superhero/hero/urls.py
+++ Superhero/hero/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
 from .views import *
+from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
+from hero import views as core_views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -10,4 +14,6 @@ urlpatterns = [
     path('home/', HomePageView.as_view()),
     path('detail/', DetailView.as_view()),
     path('addhero/', HeroEditView.as_view()),
+    path('post/<int:pk>/delete', HeroDeleteView.as_view(), name="hero_delete"),
+    
 ]
--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, View, DetailView
 from django.views.generic.edit import CreateView
 from .models import Superhero
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class PageView(TemplateView):
     def get_template_names(self):
@@ -14,16 +19,8 @@ class IndexView(TemplateView):
     
     
 class HeroView(ListView):
-    def get_template_names(self):
-        template_name = 'hero.html'
-        return template_name
-    
-    def get_context_data(self, **kwargs):
-        return{
-            'hero': self.kwargs.get('hero')
-        }
+    template_name = "hero.html"
     model = Superhero
-    context_object_name = "all_heros_list"
         
 
 class HomePageView(ListView):
@@ -37,13 +34,58 @@ class DetailView(ListView):
     model = Superhero
     context_object_name = "all_heros_list"
     
-class addHeroView(CreateView):
+class addHeroView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
     model = Superhero
     template_name = "create.html"
     context_object_name = "all_heros_list"
     fields = '__all__'
     
-class HeroEditView(CreateView):
+class HeroEditView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
     template_name = "hero_edit.html"
     model = Superhero
     fields = '__all__'
+    
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+class LoginView(View):
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+
+                return HttpResponseRedirect('/form')
+            else:
+                return HttpResponse("Inactive user.")
+        else:
+            return HttpResponseRedirect(settings.LOGIN_URL)
+
+        return render(request, "index.html")
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(settings.LOGIN_URL)
+    
+    
+class HeroDeleteView(LoginRequiredMixin, DetailView):
+    model = Superhero
+    template_name = "hero_delete.html"
